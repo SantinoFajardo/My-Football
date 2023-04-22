525080f667a9f04ab9dd32f164c469f2b191f1ad
FajardoSatino <santino@sphereone.xyz>
2023-04-22 13:27:49 -0300

    Initial Commit

---
+++ MyFootbaall/MyFootbaallApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFootbaallApp.swift
+//  MyFootbaall
+//
+//  Created by Santino Fajardo on 22/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFootbaallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
